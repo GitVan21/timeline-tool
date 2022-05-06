358a65d6f1e5ba1b43f5f9386c65bd9511fc71f3
Iván Benjamín <ivan.benjamin92@gmail.com>
2022-05-06 12:38:04 +0200

    local-storage working

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
-"use-strict"
+"use-strict";
 
-const url = "https://gist.githubusercontent.com/bertez/8e62741154903c35edb3bfb825a7f052/raw/b5cd5137fd168116cc71740f1fbb75819d0fa82e/zelda-timeline.json"
-const line = document.querySelector("#line-time")
+const url =
+  "https://gist.githubusercontent.com/bertez/8e62741154903c35edb3bfb825a7f052/raw/b5cd5137fd168116cc71740f1fbb75819d0fa82e/zelda-timeline.json";
+const line = document.querySelector("#line-time");
 
 /**
  * Esta funcion servira como llamada a la api y retorna una promesa resuelta o rechazada
  * que se tratara con un async/await
- * 
+ *
  * @direction {String} Url a la que diriges el fetch
  */
 const collectData = (direction) => {
-    return new Promise((resolve, reject) => {
-        fetch(direction)
-            .then((resp) => resp.ok ? resp.json() : reject('NO se pudo recuperar JSON'))
-            .then((data) => {
-                resolve(data)
-            })
-            .catch(err => reject(err))
-    })
-}
+  return new Promise((resolve, reject) => {
+    fetch(direction)
+      .then((resp) =>
+        resp.ok ? resp.json() : reject("NO se pudo recuperar JSON")
+      )
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => reject(err));
+  });
+};
 
 /**
  * Sirve de entrada para dibujar el contenido de la linea temporal
  */
-const drawLine = async() => {
+const drawLine = async () => {
+  let info = [];
 
-    //Recibimos la promesa de collectData y esperamos a que se resuelva o se rechaze
-    const response = await collectData(url)
-    
-    //Iteramos la cadena resultante siendo E cada elemento e I su indice
-    response.map((e, i) => {
-/*      //Creamos los elementos
+  if (info.length > 0) {
+    info = JSON.parse(localStorage.getItem("info"));
+  } else {
+    info = await collectData(url);
+    localStorage.setItem("info", JSON.stringify(info));
+    console.log("Conectando a GitHub");
+  }
+
+  //Iteramos la cadena resultante siendo E cada elemento e I su indice
+  info.map((e, i) => {
+    /*      //Creamos los elementos
         const point = document.createElement('li')
         const dialog = document.createElement('div')
 
@@ -43,16 +52,18 @@ const drawLine = async() => {
         //Añadimos el estilo de point al li antes de enlazarlo como hijo de de la line
         point.appendChild(dialog)
         line.appendChild(point) */
-        createLiAndCard(e,i)
-    })
-}
+    createLiAndCard(e, i);
+  });
+};
 
-const createLiAndCard = (element,index) => {
+const createLiAndCard = (element, index) => {
   const li = document.createElement("li");
-  li.classList.add('point-time')
+  li.classList.add("point-time");
 
-    li.innerHTML = `
-    <div class="body-card dialog ${index%2 === 0 ? 'dialog-left' : 'dialog-right'}">
+  li.innerHTML = `
+    <div class="body-card dialog ${
+      index % 2 === 0 ? "dialog-left" : "dialog-right"
+    }">
       <figure class="frame">
         <img
           src=${element.image}
@@ -67,27 +78,28 @@ const createLiAndCard = (element,index) => {
     </div>
     `;
 
-    line.append(li);
-}
+  line.append(li);
+};
 
 drawLine();
 
 //Añadimos a la ventana un 'escuchador' para el evento scroll
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
+  //Recogemos todos los dialogs y los convertimos a un Array para poder manipularlos
+  const dialogs = Array.prototype.slice.call(
+    document.querySelectorAll(".dialog")
+  );
+  //Recogemos la altura total de la ventana para usarla de referencia
+  const screenSize = window.innerHeight;
 
-    //Recogemos todos los dialogs y los convertimos a un Array para poder manipularlos
-    const dialogs = Array.prototype.slice.call(document.querySelectorAll('.dialog'))
-    //Recogemos la altura total de la ventana para usarla de referencia
-    const screenSize = window.innerHeight;
-    
-    //Iteramos: getBoundingClientRect().top sacara la altura a la que se encuentra la pantalla y screenSize recogera su altura total
-    dialogs.map((e, i) => {
-        if(e.getBoundingClientRect().top < screenSize){
-            e.classList.add('visible')
-            e.classList.remove('no-visible')
-        }else{
-            e.classList.add('no-visible')
-            e.classList.remove('visible')
-        }
-    })
-})
+  //Iteramos: getBoundingClientRect().top sacara la altura a la que se encuentra la pantalla y screenSize recogera su altura total
+  dialogs.map((e, i) => {
+    if (e.getBoundingClientRect().top < screenSize) {
+      e.classList.add("visible");
+      e.classList.remove("no-visible");
+    } else {
+      e.classList.add("no-visible");
+      e.classList.remove("visible");
+    }
+  });
+});
